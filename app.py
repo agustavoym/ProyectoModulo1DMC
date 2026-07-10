c3d76294ea574163e01938ce3c50738d812cfd50
agustavoym <agustavoym@gmail.com>
2026-07-09 22:20:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ st.sidebar.title("Parámetros")
 st.image("Python_logo.png")
 st.sidebar.image("DMC.png")
 
+modulo = st.sidebar.selectbox("Elija un módulo", ["Módulo Listas","Módulo Array","Módulo funciones")
+
+if modulo == "Módulo Listas":
 
 valor_inicial = int(st.number_input("Ingrese el valor inicial", value=0))
 valor_final = int(st.number_input("Ingrese el valor final", value=1))
@@ -13,3 +16,9 @@ valor_final = int(st.number_input("Ingrese el valor final", value=1))
 lista_numerica = list(range(valor_inicial, valor_final))
 
 st.write(lista_numerica)
+
+if modulo = "Módulo Array":
+st.write("Estas en el módulo de arreglos"
+
+else 
+st.write("Estas en el módulo de funciones"
